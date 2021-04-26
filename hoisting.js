3dcc972162a043d107dc65fa93699563bdc14b0f
Zachary Goodman <zacharyrgoodman@gmail.com>
2021-04-26 15:40:55 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -10,11 +10,12 @@ function callMe() {
 
 function crazy() {
   // fix the code in here:
-  thisIsCrazy();
+  
 
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
+  thisIsCrazy();
 }
 
 function sayMyName() {
